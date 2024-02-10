aab4e62ed9173cd89bf81a3f5df047e60f173d96
GuiDev45 <bortolettodev@gmail.com>
2024-02-10 15:41:00 -0300

    Etapa 1 - Formulário simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-// Planejamento formulário simples
+import { useState } from "react";
+import "./index.css"; // Importe seu arquivo CSS aqui
 
-function App() {
-  // Criar os hooks de estado para o email, senha e erros []
-  // Criar as funções de handle/manipulação, que vão controlar os inputs de email, senha e erros []
-  // Fazer o render/return do formulário, que terá um botão para submit []
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    console.log("Submissão feita corretamente");
+  };
+
   return (
-    <div>
-      <p>App</p>
+    <div className="container-flex">
+      <form className="container-flex-form" onSubmit={handleSubmit}>
+        <h1>Formulário Simples</h1>
+        <input
+          type="text"
+          placeholder="Email:"
+          value={email}
+          onChange={handleChangeEmail}
+        />
+        <input
+          type="password"
+          placeholder="Password:"
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
-
-export default App;
