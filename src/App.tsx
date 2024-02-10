5a5fdf93bce250ed398dbe229713dcf4fd3f1ae8
GuiDev45 <bortolettodev@gmail.com>
2024-02-10 11:56:19 -0300

    Planejamento formulário simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+// Planejamento formulário simples
 
 function App() {
-
-
+  // Criar os hooks de estado para o email, senha e erros []
+  // Criar as funções de handle/manipulação, que vão controlar os inputs de email, senha e erros []
+  // Fazer o render/return do formulário, que terá um botão para submit []
   return (
     <div>
       <p>App</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
