f7f7bc8e14bbc97302e68f827991172f2615b433
GuiDev45 <bortolettodev@gmail.com>
2024-02-11 18:48:45 -0300

    Etapa 4 - Criação do formulário usando o react-hook-form, sem usar validações

--- src/pages/RHFform/index.tsx
+++ src/pages/RHFform/index.tsx
@@ -1,13 +1,41 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import "./index.css";
 
+type FormFields = {
+  email: string;
+  password: string;
+};
+
 export default function RHFform() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: {
+      email: "teste@valorPadrao.com",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
   return (
-    <div className="container-flex">
+    <div className="container-flex" onSubmit={handleSubmit(onSubmit)}>
       <form className="container-flex-form">
         <h1>Formulário RHF</h1>
-        <input type="text" placeholder="Email:" />
-        <input type="password" placeholder="Password:" />
-        <button type="submit">Enviar</button>
+        <input {...register("email")} type="text" placeholder="Email:" />
+        <input
+          {...register("password")}
+          type="password"
+          placeholder="Password:"
+        />
+        <button disabled={isSubmitting} type="submit">
+          {isSubmitting ? "Loading..." : "Submit"}
+        </button>
       </form>
     </div>
   );
